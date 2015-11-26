dc060cc06c1fd2a7ad2fe08e6117878b5edffcf9
Ryan Brewer <webbrewer@gmail.com>
2015-11-26 14:44:00 -0600

    character editor

--- app/components/character-editor.js
+++ app/components/character-editor.js
@@ -1,30 +1,30 @@
 CharacterList = new Mongo.Collection('character');
 /*
-name, hp, mp, strength, defense, agility, luck, createdBy
+ name, hp, mp, strength, defense, agility, luck, createdBy
  */
 if (Meteor.isClient) {
   Template.characterEditor.helpers({
-    characterList: function () {
-      return CharacterList.find({}, {sort:{name:1}});
+    characterList:     function () {
+      return CharacterList.find({}, {sort: {name: 1}});
     },
-    selectedCharacter:function(){
+    selectedCharacter: function () {
       var selectedCharacter = Session.get('character');
       console.log('selected character: ', selectedCharacter);
       return CharacterList.findOne(selectedCharacter);
     }
   });
   Template.characterEditor.events({
-    'click .js-character-selector__character':function(){
+    'click .js-character-selector__character': function () {
       var characterId = this._id;
       Session.set('character', characterId);
     },
-    'click .js-insert-new-character': function () {
-      Meteor.call('insertNewCharacter', function(result){
+    'click .js-insert-new-character':          function () {
+      Meteor.call('insertNewCharacter', function (result) {
         Session.set('character', result);
       });
       //TODO select nav item for newly created character. The 'active' class should be set by the Session character model
     },
-    'submit .js-update-character-data':         function (event) {
+    'submit .js-update-character-data':        function (event) {
       Meteor.call('updateCharacterData', {
         name:    event.target.name.value,
         hp:      event.target.hp.value,
@@ -54,13 +54,13 @@ if (Meteor.isServer) {
     },
     updateCharacterData: function (data) {
       CharacterList.update({
-        name: document.characterEditor.name.value,
-        hp: document.characterEditor.hp.value,
-        mp: document.characterEditor.mp.value,
+        name:     document.characterEditor.name.value,
+        hp:       document.characterEditor.hp.value,
+        mp:       document.characterEditor.mp.value,
         strength: document.characterEditor.strength.value,
-        defense: document.characterEditor.defense.value,
-        agility: document.characterEditor.agility.value,
-        luck: document.characterEditor.luck.value
+        defense:  document.characterEditor.defense.value,
+        agility:  document.characterEditor.agility.value,
+        luck:     document.characterEditor.luck.value
       });
     }
   })
